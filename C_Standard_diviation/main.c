8769544df9f8fb204f58c67b472aa037d6f66b0c
Park JaiChang <parkghoxzs@naver.com>
2016-07-22 12:16:03 +0900

    Initial Commit

---
+++ C_Standard_diviation/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  C_Standard_diviation
+//
+//  Created by PARK JAICHANG on 7/22/16.
+//  Copyright © 2016 JAICHANGPARK. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
